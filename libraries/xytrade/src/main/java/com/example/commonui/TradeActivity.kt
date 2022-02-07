1a15f5da0377c76f7c92353412f3e4b0a93ed018
jingqingqing <jingqingqing@xueqiu.com>
2022-02-07 11:32:52 +0800

    初始化工程

---
+++ libraries/xytrade/src/main/java/com/example/commonui/TradeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.commonui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TradeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
